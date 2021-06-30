52d6fb395c1bbcfd212849b227b1e4c340f0f3ac
Kevin Lewi <klewi@fb.com>
2021-06-30 14:01:16 -0700

    Adding licenses + headers

--- src/seemless_directory.rs
+++ src/seemless_directory.rs
@@ -1,3 +1,8 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
 use crate::append_only_zks::{AppendOnlyProof, NonMembershipProof};
 use crate::errors::SeemlessDirectoryError;
 use crate::{
