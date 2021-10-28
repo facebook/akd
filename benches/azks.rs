ed50627fb8b34459a576e69bdd516b4a5b4dfeb5
Sean Lawlor <seanlawlor@fb.com>
2021-10-28 13:40:44 -0400

    fix the benchmarks for async IO

--- benches/azks.rs
+++ benches/azks.rs
@@ -17,15 +17,18 @@ use winter_math::fields::f128::BaseElement;
 
 type Blake3 = Blake3_256<BaseElement>;
 type Blake3Digest = <Blake3_256<winter_math::fields::f128::BaseElement> as Hasher>::Digest;
-type InMemoryDb = seemless::storage::memory::InMemoryDatabase;
+type InMemoryDb = seemless::storage::memory::r#async::AsyncInMemoryDatabase;
 
 fn single_insertion(c: &mut Criterion) {
     let num_nodes = 1000;
 
     let mut rng: ThreadRng = thread_rng();
 
+    let runtime = tokio::runtime::Runtime::new().unwrap();
+
     let db = InMemoryDb::new();
-    let mut azks1 = Azks::<Blake3, InMemoryDb>::new(&db, &mut rng).unwrap();
+
+    let mut azks1 = runtime.block_on(Azks::<Blake3, InMemoryDb>::new(&db, &mut rng)).unwrap();
     let mut insertion_set = Vec::<(NodeLabel, Blake3Digest)>::new();
     for _ in 0..num_nodes {
         let node = NodeLabel::random(&mut rng);
@@ -33,8 +36,9 @@ fn single_insertion(c: &mut Criterion) {
         rng.fill_bytes(&mut input);
         let val = Blake3::hash(&input);
         insertion_set.push((node, val));
-        azks1.insert_leaf(&db, node, val).unwrap();
+        runtime.block_on(azks1.insert_leaf(&db, node, val)).unwrap();
     }
+
     c.bench_function("single insertion into tree with 1000 nodes", move |b| {
         b.iter(|| {
             let node = NodeLabel::random(&mut rng);
@@ -43,7 +47,7 @@ fn single_insertion(c: &mut Criterion) {
             let val = Blake3::hash(&input);
 
             let _start = Instant::now();
-            azks1.insert_leaf(&db, node, val).unwrap();
+            runtime.block_on(azks1.insert_leaf(&db, node, val)).unwrap();
         })
     });
 }
