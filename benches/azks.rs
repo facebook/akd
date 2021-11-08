3eec5a1fbf8f1b0fefbbff344a794d9cc591f327
Kevin Lewi <klewi@fb.com>
2021-11-08 11:27:15 -0800

    Fixing benchmark compilation

--- benches/azks.rs
+++ benches/azks.rs
@@ -24,7 +24,7 @@ fn single_insertion(c: &mut Criterion) {
 
     let mut rng: ThreadRng = thread_rng();
 
-    let runtime = tokio::runtime::Runtime::new().unwrap();
+    let mut runtime = tokio::runtime::Runtime::new().unwrap();
 
     let db = akd::storage::V2FromV1StorageWrapper::new(InMemoryDb::new());
 
