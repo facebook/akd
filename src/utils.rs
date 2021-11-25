af8aa68c54d2e46e3d3b20e93d94921240262d94
Kevin Lewi <klewi@fb.com>
2021-11-25 13:27:23 -0800

    Adding utils.rs

---
+++ src/utils.rs
@@ -0,0 +1,24 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under both the MIT license found in the
+// LICENSE-MIT file in the root directory of this source tree and the Apache
+// License, Version 2.0 found in the LICENSE-APACHE file in the root directory
+// of this source tree.
+
+// 1. Create a hashmap of all prefixes of all elements of the insertion set
+// 2. For each node in current_nodes set, check if each child is in prefix hashmap
+// 3. If so, add child loc to batch set
+
+use crate::node_state::NodeLabel;
+use std::collections::HashSet;
+
+// Builds a set of all prefixes of the input labels
+pub(crate) fn build_prefixes_set(labels: &[NodeLabel]) -> HashSet<NodeLabel> {
+    let mut prefixes_set = HashSet::new();
+    for label in labels {
+        for len in 0..(label.get_len() + 1) {
+            prefixes_set.insert(label.get_prefix(len));
+        }
+    }
+    prefixes_set
+}
