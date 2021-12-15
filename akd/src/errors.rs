07ca4c8afd73b9b51df6fbb6cfc08e9619fee8c8
Ercan Ozturk <eoz@fb.com>
2021-12-14 19:35:03 -0800

    Remove unused errors

--- akd/src/errors.rs
+++ akd/src/errors.rs
@@ -72,25 +72,11 @@ pub enum HistoryTreeNodeError {
     DirectionIsNone,
     /// The node didn't have a child in the given epoch
     NoChildInTreeAtEpoch(u64, usize),
-    /// The node had no children at the given epoch
-    NoChildrenInTreeAtEpoch(u64),
-    /// The hash was being updated for an invalid epoch
-    InvalidEpochForUpdatingHash(u64),
-    /// Tried to update the parent of the root, which should not be done
-    TriedToUpdateParentOfRoot,
     /// The next epoch of this node's parent was invalid
     ParentNextEpochInvalid(u64),
     /// The hash of a parent was attempted to be updated, without setting the calling node as a child.
     HashUpdateOnlyAllowedAfterNodeInsertion,
     /// The children of a leaf are always dummy and should not be hashed
-    TriedToHashLeafChildren,
-    /// The list of epochs for a given node was empty
-    NodeCreatedWithoutEpochs(u64),
-    /// The label of a leaf node was shorter than that of an interior node.
-    LeafNodeLabelLenLessThanInterior(NodeLabel),
-    /// Error compressing the Merkle trie
-    CompressionError(NodeLabel),
-    /// Tried to access something about the node at an epoch that didn't exist.
     NodeDidNotExistAtEp(NodeLabel, u64),
     /// The state of a node did not exist at a given epoch
     NodeDidNotHaveExistingStateAtEp(NodeLabel, u64),
@@ -110,20 +96,11 @@ impl fmt::Display for HistoryTreeNodeError {
                     node_label, child_label
                 )
             }
-            Self::NoChildrenInTreeAtEpoch(epoch) => {
-                write!(f, "no children at epoch {}", epoch)
-            }
             Self::NoChildInTreeAtEpoch(epoch, direction) => {
                 write!(f, "no node in direction {} at epoch {}", direction, epoch)
             }
             Self::DirectionIsNone => {
                 write!(f, "Direction provided is None")
-            }
-            Self::InvalidEpochForUpdatingHash(epoch) => {
-                write!(f, "Invalid epoch for updating hash {}", epoch)
-            }
-            Self::TriedToUpdateParentOfRoot => {
-                write!(f, "Tried to update parent of root")
             }
             Self::ParentNextEpochInvalid(epoch) => {
                 write!(f, "Next epoch of parent is invalid, epoch = {}", epoch)
@@ -132,22 +109,6 @@ impl fmt::Display for HistoryTreeNodeError {
                 write!(
                     f,
                     "Hash update in parent only allowed after node is inserted"
-                )
-            }
-            Self::TriedToHashLeafChildren => {
-                write!(f, "Tried to hash the children of a leaf")
-            }
-            Self::NodeCreatedWithoutEpochs(label) => {
-                write!(f, "A node exists which has no epochs. Nodes should always have epochs, labelled: {}", label)
-            }
-            Self::LeafNodeLabelLenLessThanInterior(label) => {
-                write!(f, "A leaf was inserted with lable length shorter than an interior node, labelled: {:?}", label)
-            }
-            Self::CompressionError(label) => {
-                write!(
-                    f,
-                    "A node without a child in some direction exists, labelled: {:?}",
-                    label
                 )
             }
             Self::NodeDidNotExistAtEp(label, epoch) => {
@@ -208,16 +169,12 @@ impl fmt::Display for AzksError {
 /// The errors thrown by various algorithms in [crate::directory::Directory]
 #[derive(Debug)]
 pub enum DirectoryError {
-    /// Tried to audit for "append-only" from epoch a to b where a > b
-    AuditProofStartEpLess(u64, u64),
     /// Looked up a user not in the directory
     LookedUpNonExistentUser(String, u64),
     /// Lookup proof did not verify
     LookupVerificationErr(String),
     /// Key-History proof did not verify
     KeyHistoryVerificationErr(String),
-    /// Error generating the key history proof
-    KeyHistoryProofErr(String),
     /// Error propogation
     StorageError,
 }
@@ -225,14 +182,6 @@ pub enum DirectoryError {
 impl fmt::Display for DirectoryError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Self::AuditProofStartEpLess(start, end) => {
-                write!(
-                    f,
-                    "Audit proof requested for epoch {:?} till {:?} and the audit start epoch is greater than or equal to the end.",
-                    start,
-                    end
-                )
-            }
             Self::StorageError => {
                 write!(f, "Error with retrieving value from storage")
             }
@@ -243,9 +192,6 @@ impl fmt::Display for DirectoryError {
                 write!(f, "{}", err_string)
             }
             Self::LookupVerificationErr(err_string) => {
-                write!(f, "{}", err_string)
-            }
-            Self::KeyHistoryProofErr(err_string) => {
                 write!(f, "{}", err_string)
             }
         }
@@ -259,8 +205,6 @@ pub enum StorageError {
     SetError(String),
     /// An error occurred getting data from the storage layer
     GetError(String),
-    /// An error occurred serializing or deserializing data
-    SerializationError,
-    /// Some kind of storage connection error occurred
+        /// Some kind of storage connection error occurred
     Connection(String),
 }
