18645e98f6a587b54f5e6a0e4d1c3a527b1cff47
Harjasleen Malvai <hmalvai@fb.com>
2022-06-21 14:58:43 -0700

    Re-ran linting

--- akd_test_tools/src/fixture_generator/examples/example_tests.rs
+++ akd_test_tools/src/fixture_generator/examples/example_tests.rs
@@ -17,8 +17,8 @@ use akd::{
 use winter_crypto::hashers::Blake3_256;
 use winter_math::fields::f128::BaseElement;
 
+use crate::fixture_generator::reader::yaml::YamlFileReader;
 use crate::fixture_generator::reader::Reader;
-use crate::fixture_generator::{reader::yaml::YamlFileReader};
 
 type Blake3 = Blake3_256<BaseElement>;
 
