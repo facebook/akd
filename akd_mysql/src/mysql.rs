afcdf29e7d30029ac64a42d7f055622e9e0f919f
Ercan Ozturk <eoz@fb.com>
2022-08-15 12:40:24 -0700

    Remove debug message

--- akd_mysql/src/mysql.rs
+++ akd_mysql/src/mysql.rs
@@ -911,10 +911,6 @@ impl Storage for AsyncMySqlDatabase {
         )
         .await;
 
-        if St::data_type() == StorageType::TreeNode {
-            println!("Get direct tree node.");
-        }
-
         debug!("BEGIN MySQL get {:?}", id);
         let result = async {
             let tic = Instant::now();
