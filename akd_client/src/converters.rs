6ecb30ce6e7954ea72667e1f4b5051323ba636a0
Ercan Ozturk <eoz@fb.com>
2022-09-07 10:59:48 -0700

    Update copyright header

--- akd_client/src/converters.rs
+++ akd_client/src/converters.rs
@@ -1,4 +1,4 @@
-// Copyright (c) Facebook, Inc. and its affiliates.
+// Copyright (c) Meta, Inc. and its affiliates.
 //
 // This source code is licensed under both the MIT license found in the
 // LICENSE-MIT file in the root directory of this source tree and the Apache
