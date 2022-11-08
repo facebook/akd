b507ec016f560c9d1dbf22140d394b20d94942c3
Sean Lawlor <seanlawlor@fb.com>
2022-11-08 11:25:55 -0500

    Upgrade protobuf to version 3.2.0

--- akd/src/build.rs
+++ akd/src/build.rs
@@ -10,8 +10,6 @@
 
 // NOTE: build.rs documentation = https://doc.rust-lang.org/cargo/reference/build-scripts.html
 
-extern crate protoc_rust;
-
 /// The list of protobuf files to generate
 const PROTOBUF_FILES: [&str; 1] = ["src/proto/audit"];
 
@@ -22,15 +20,19 @@ fn build_protobuf(file: &str) {
     println!("cargo:rerun-if-changed={}.proto", file);
 
     // compile the file
-    protoc_rust::run(protoc_rust::Args {
-        out_dir: "src/proto",
-        input: &[&proto_file],
-        includes: &[],
-        customize: protoc_rust::Customize {
-            ..Default::default()
-        },
-    })
-    .expect("protoc");
+    protobuf_codegen::Codegen::new()
+        // use protoc parser, (optional)
+        .protoc()
+        // Use `protoc-bin-vendored` bundled protoc command, optional.
+        .protoc_path(&protoc_bin_vendored::protoc_bin_path().unwrap())
+        // All inputs and imports from the inputs must reside in `includes` directories.
+        .includes(&["src/proto"])
+        // Inputs must reside in some of the include paths
+        .input(&proto_file)
+        // Specify output directory relative to Cargo output directory
+        .cargo_out_dir("proto")
+        .run()
+        .expect("protoc");
 }
 
 fn build_protobufs() {
