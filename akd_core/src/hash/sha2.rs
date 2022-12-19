29ae3127f2f7dd54f714e0929f0a32beb5916d36
Sean Lawlor <seanlawlor@meta.com>
2022-12-19 12:20:40 -0500

    Support for SHA512/256

--- akd_core/src/hash/sha2.rs
+++ akd_core/src/hash/sha2.rs
@@ -15,6 +15,10 @@ pub const DIGEST_BYTES: usize = 32;
 /// The number of bytes in a digest for SHA2 512 hashes
 #[cfg(feature = "sha512")]
 pub const DIGEST_BYTES: usize = 64;
+/// The number of bytes in a digest for the truncated SHA2 512 hashes.
+/// This means we take the first 32 bytes of a 64 byte hash.
+#[cfg(feature = "sha512_256")]
+pub const DIGEST_BYTES: usize = 32;
 
 /// Hash a single byte array
 pub fn hash(item: &[u8]) -> crate::hash::Digest {
@@ -22,6 +26,9 @@ pub fn hash(item: &[u8]) -> crate::hash::Digest {
     let hash = sha2::Sha256::digest(item);
     #[cfg(feature = "sha512")]
     let hash = sha2::Sha512::digest(item);
+    #[cfg(feature = "sha512_256")]
+    let hash = sha2::Sha512_256::digest(item);
+
     if hash.len() == DIGEST_BYTES {
         // OK
         let ptr = hash.as_ptr() as *const [u8; DIGEST_BYTES];
