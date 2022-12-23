97c4da1ca22ea1bf3241ae77132011f647b906a2
Evan Au <evanau@fb.com>
2022-12-24 06:14:10 +0800

    Remove duplicated test suite

--- integration_tests/src/test_util.rs
+++ integration_tests/src/test_util.rs
@@ -9,8 +9,8 @@ extern crate thread_id;
 
 use akd::ecvrf::VRFKeyStorage;
 use akd::storage::StorageManager;
+use akd::Directory;
 use akd::{AkdLabel, AkdValue};
-use akd::{Directory, HistoryParams};
 use log::{info, Level, Metadata, Record};
 use once_cell::sync::OnceCell;
 use rand::distributions::Alphanumeric;
@@ -119,132 +119,6 @@ impl log::Log for FileLogger {
 }
 
 // ================== Test Helpers ================== //
-
-// FIXME: We actually probably want to use this. Figure out later.
-/// The suite of tests to run against a fully-instantated and storage-backed directory.
-/// This will publish 3 epochs of ```num_users``` records and
-/// perform 10 random lookup proofs + 2 random history proofs + and audit proof from epochs 1u64 -> 2u64
-#[allow(unused)]
-pub(crate) async fn directory_test_suite<
-    S: akd::storage::Database + Sync + Send,
-    V: VRFKeyStorage,
->(
-    mysql_db: &StorageManager<S>,
-    num_users: usize,
-    vrf: &V,
-) {
-    // generate the test data
-    let mut rng = thread_rng();
-
-    let mut users: Vec<String> = vec![];
-    for _ in 0..num_users {
-        users.push(
-            thread_rng()
-                .sample_iter(&Alphanumeric)
-                .take(30)
-                .map(char::from)
-                .collect(),
-        );
-    }
-
-    let mut root_hashes = vec![];
-
-    // create & test the directory
-    let maybe_dir = Directory::<_, _>::new(mysql_db, vrf, false).await;
-    match maybe_dir {
-        Err(akd_error) => panic!("Error initializing directory: {:?}", akd_error),
-        Ok(dir) => {
-            info!("AKD Directory started. Beginning tests");
-
-            // Publish 3 epochs of user material
-            for i in 1..=3 {
-                let mut data = Vec::new();
-                for value in users.iter() {
-                    data.push((
-                        AkdLabel::from_utf8_str(value),
-                        AkdValue(format!("{}", i).as_bytes().to_vec()),
-                    ));
-                }
-
-                if let Err(error) = dir.publish(data).await {
-                    panic!("Error publishing batch {:?}", error);
-                } else {
-                    info!("Published epoch {}", i);
-                }
-                let azks = dir.retrieve_current_azks().await.unwrap();
-                root_hashes.push(dir.get_root_hash(&azks).await);
-            }
-
-            // Perform 10 random lookup proofs on the published users
-            for user in users.iter().choose_multiple(&mut rng, 10) {
-                let key = AkdLabel::from_utf8_str(user);
-                match dir.lookup(key.clone()).await {
-                    Err(error) => panic!("Error looking up user information {:?}", error),
-                    Ok((proof, root_hash)) => {
-                        let vrf_pk = dir.get_public_key().await.unwrap();
-                        if let Err(error) = akd::client::lookup_verify(
-                            vrf_pk.as_bytes(),
-                            root_hash.hash(),
-                            key,
-                            proof,
-                        ) {
-                            panic!("Lookup proof failed to verify {:?}", error);
-                        }
-                    }
-                }
-            }
-            info!("10 random lookup proofs passed");
-
-            // Perform 2 random history proofs on the published material
-            for user in users.iter().choose_multiple(&mut rng, 2) {
-                let key = AkdLabel::from_utf8_str(user);
-                match dir.key_history(&key, HistoryParams::default()).await {
-                    Err(error) => panic!("Error performing key history retrieval {:?}", error),
-                    Ok((proof, root_hash)) => {
-                        let vrf_pk = dir.get_public_key().await.unwrap();
-                        if let Err(error) = akd::client::key_history_verify(
-                            vrf_pk.as_bytes(),
-                            root_hash.hash(),
-                            root_hash.epoch(),
-                            key,
-                            proof,
-                            akd::HistoryVerificationParams::default(),
-                        ) {
-                            panic!("History proof failed to verify {:?}", error);
-                        }
-                    }
-                }
-            }
-
-            info!("2 random history proofs passed");
-
-            // Perform an audit proof from 1u64 -> 2u64
-            match dir.audit(1u64, 2u64).await {
-                Err(error) => panic!("Error perform audit proof retrieval {:?}", error),
-                Ok(proof) => {
-                    let start_root_hash = root_hashes[0].as_ref();
-                    let end_root_hash = root_hashes[1].as_ref();
-                    match (start_root_hash, end_root_hash) {
-                        (Ok(start), Ok(end)) => {
-                            if let Err(error) =
-                                akd::auditor::audit_verify(vec![*start, *end], proof).await
-                            {
-                                panic!("Error validating audit proof {:?}", error);
-                            }
-                        }
-                        (Err(err), _) => {
-                            panic!("Error retrieving root hash at epoch {:?}", err);
-                        }
-                        (_, Err(err)) => {
-                            panic!("Error retrieving root hash at epoch {:?}", err);
-                        }
-                    }
-                }
-            }
-            info!("Audit proof from 1u64 -> 2u64 passed");
-        }
-    }
-}
 
 pub(crate) async fn test_lookups<S: akd::storage::Database + Sync + Send, V: VRFKeyStorage>(
     mysql_db: &StorageManager<S>,
